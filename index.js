5c7f3a2bee6b002c588d5e18de82726d79cf958a
Seth <seth.bbrendle@gmail.com>
2022-08-30 17:27:45 -0400

    commit tree a16d5b1a2d322403fe150ae0c4b1ffdce9724320

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = () => {
+    let a = 2000
+    let b = 100
+    return a / b;
+}
+
+const square = (param) => {
+    let x = 2;
+    return (param) * (param);
+}
+
+const add = (a, b) => {
+    return (a) + (b);
+}
